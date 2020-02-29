c48742d2943c609b83df505c8e4f51b00d7f2932
Mazhar WPC <mazharshaikh94@gmail.com>
2020-03-01 01:56:12 +0900

    Added final changes

--- main.py
+++ main.py
@@ -66,13 +66,14 @@ test_loader = torch.utils.data.DataLoader(
     batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, hidden_size):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
+        self.fc1 = nn.Linear(320, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, 10)
+        self.hidden_size = hidden_size
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
